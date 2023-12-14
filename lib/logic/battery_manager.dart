389434c204425ac4d7891bbd8fdf8735eb4afa28
lbaum <lbaumgaertner@culba.de>
2023-12-14 15:45:37 +0100

    listen to battery change only when batteryWidget is enabled

--- lib/logic/battery_manager.dart
+++ lib/logic/battery_manager.dart
@@ -1,25 +1,57 @@
+import 'dart:async';
+
 import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:mars_launcher/logic/settings_manager.dart';
+import 'package:mars_launcher/services/service_locator.dart';
 
 class BatteryManager {
+  final settingsManager = getIt<SettingsManager>();
   final Battery _battery = Battery();
   final batteryLevelNotifier = ValueNotifier(0);
 
+  StreamSubscription<BatteryState>? subscriptionBatteryEnabled;
+
   BatteryManager() {
-    _battery.onBatteryStateChanged.listen((BatteryState state) {
-      print('Battery state changed: $state');
-      updateBatteryLevel();
-    });
+    settingsManager.batteryWidgetEnabledNotifier.addListener(handleBatteryWidgetEnabledChanged);
+
+    /// Activate listener if batteryWidget is enabled
+    if (settingsManager.batteryWidgetEnabledNotifier.value) {
+      activateOnBatteryChangedListener();
+    }
+  }
+
+  void handleBatteryWidgetEnabledChanged() {
+    if (settingsManager.batteryWidgetEnabledNotifier.value) {
+      activateOnBatteryChangedListener();
+    } else {
+      deactivateOnBatteryChangedListener();
+    }
+  }
+
+  /// Listen to changes of battery
+  void activateOnBatteryChangedListener() {
+    if (subscriptionBatteryEnabled == null) {
+      /// Add subscription if not already subscribed
+      subscriptionBatteryEnabled = _battery.onBatteryStateChanged.listen((BatteryState state) {
+        print('Battery state changed: $state');
+        updateBatteryLevel();
+      });
+    }
+  }
+
+  /// Cancel subscription to battery changed
+  void deactivateOnBatteryChangedListener() {
+    subscriptionBatteryEnabled?.cancel();
+    subscriptionBatteryEnabled = null;
   }
 
   Future<void> updateBatteryLevel() async {
-
     _battery.batteryLevel.then((level) {
       if (level != batteryLevelNotifier.value) {
-      print("BATTERY LEVEL: $level");
+        print("BATTERY LEVEL: $level");
         batteryLevelNotifier.value = level;
       }
     });
   }
-
 }
